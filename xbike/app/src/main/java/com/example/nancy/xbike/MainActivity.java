61eed7959066fd20e7e0a5f6795e323d96ff50a1
Nancy Wang <Nancy@lawn-128-61-35-210.lawn.gatech.edu>
2017-01-15 18:51:17 -0500

    side bar

--- xbike/app/src/main/java/com/example/nancy/xbike/MainActivity.java
+++ xbike/app/src/main/java/com/example/nancy/xbike/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_unlockBike) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_payment) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
